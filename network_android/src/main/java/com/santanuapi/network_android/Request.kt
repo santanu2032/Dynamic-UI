cd9cd7ae8fa1359d39ff2565dbbbc0a44c507784
santanu2032 <santanusarkar2032@gmail.com>
2026-08-22 00:49:25 +0530

    added:network_module specifically  for android , now network_activity is possible.
    added:firebase and Server_key object to store key

--- network_android/src/main/java/com/santanuapi/network_android/Request.kt
+++ network_android/src/main/java/com/santanuapi/network_android/Request.kt
@@ -1,2 +1,17 @@
 package com.santanuapi.network_android
 
+import android.content.Context
+import com.google.firebase.FirebaseApp
+
+fun request(context: Context) {
+
+    print("Initiating server link\n.\n.\n.\n.\n.\n.")
+
+    try{
+        FirebaseApp.initializeApp(context,Server_KEY.key)
+    }
+    catch(e: Exception) {
+        e.printStackTrace()
+        println("Error: ${e.message}")
+    }
+}
---
+++ sharedUI/src/jvmMain/kotlin/com/sharedui/Platform.jvm.kt
@@ -0,0 +1,5 @@
+package com.sharedui
+
+actual fun platform(): String {
+    TODO("Not yet implemented")
+}
